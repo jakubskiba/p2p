528a17a00c9544ffc072a75c8c2a64948f646c8f
Maciej Nowak <mcnowak77@gmail.com>
2018-03-13 09:29:05 +0100

    Implement ChunkSender and ChunkReceiver

---
+++ src/main/java/model/ChunkRequest.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.io.Serializable;
+
+public class ChunkRequest implements Serializable{
+    private long from;
+    private long to;
+
+    public ChunkRequest(long from, long to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public long getFrom() {
+        return from;
+    }
+
+    public void setFrom(long from) {
+        this.from = from;
+    }
+
+    public long getTo() {
+        return to;
+    }
+
+    public void setTo(long to) {
+        this.to = to;
+    }
+}
