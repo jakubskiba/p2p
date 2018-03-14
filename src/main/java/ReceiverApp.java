452375f85c3be8e2acfc825313161b5a7d03ff17
jakubskiba <jakubskibakrk@gmail.com>
2018-03-14 16:27:53 +0100

    implement proof of concept chunks can be send through network and merged

--- src/main/java/ReceiverApp.java
+++ src/main/java/ReceiverApp.java
@@ -1,23 +1,29 @@
 import model.Chunk;
 import model.Sourcefile;
+import network.ChunkReceiver;
 import service.ChunkManager;
-import service.ChunkReceiver;
 import service.FileMerger;
 
 import java.io.*;
 
 public class ReceiverApp {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("sf.ser"));
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("/home/kyubu/sf.ser"));
         Sourcefile sf = (Sourcefile) ois.readObject();
-        ois.close();
+//        ois.close();
 
         ChunkManager chunkManager = new ChunkManager();
-        ChunkReceiver chunkReceiver = new ChunkReceiver(8000, "192.168.10.77");
+        ChunkReceiver chunkReceiver = new ChunkReceiver( "localhost", 12345);
+
         int chunkAmount = sf.getChunkAmount();
-        for(int i = 0; i< chunkAmount; i++) {
-            Chunk receivedChunk = chunkReceiver.getChunk(sf, i);
-            chunkManager.saveChunk(receivedChunk);
+        for(int i = 0; i<= chunkAmount; i++) {
+            System.out.printf("receiving chunk %d of %d%n", i, chunkAmount);
+            Chunk receivedChunk = chunkReceiver.receive(sf, i);
+            if(receivedChunk != null && receivedChunk.isValid()) {
+                chunkManager.saveChunk(receivedChunk);
+            } else {
+                i--;
+            }
         }
 
         new FileMerger().mergeChunks(sf.getSha256(), "ubuntu.iso");
