4987bf312d3ca49d735b83fe563edb0cbbc6b2b7
jakubskiba <jakubskibakrk@gmail.com>
2018-03-15 04:09:11 +0100

    implement App class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,43 +1,30 @@
+import controller.DownloadController;
+import controller.MainController;
+import controller.PeerController;
 import controller.SourcefileController;
 import model.Chunk;
 import model.Sourcefile;
 import service.ChunkManager;
 import service.FileDivider;
 import service.FileMerger;
+import view.UI;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        SourcefileController sfController = new SourcefileController();
-        sfController.loadSourcefiles();
-        sfController.getAll().forEach(System.out::println);
-
-        String fromPath = "/home/kyubu/DSC08863.JPG";
-        String toPath = "/home/kyubu/DSC08863-x.JPG";
-
-        File file = new File(fromPath);
-        Sourcefile sourcefile = new Sourcefile(file);
-
-
-        //divide file
-        FileDivider fileDivider = new FileDivider(sourcefile);
+        SourcefileController sourcefileController = new SourcefileController();
+        PeerController peerController = new PeerController(sourcefileController);
         ChunkManager chunkManager = new ChunkManager();
-        for(Integer i = 0; i<sourcefile.getChunkAmount(); i++) {
-            Chunk chunk = fileDivider.createChunk(i);
-            chunkManager.saveChunk(chunk);
-        }
-
-        //merge file
-        if(chunkManager.hasAllChunks(sourcefile)) {
-            new FileMerger().mergeChunks(sourcefile.getSha256(), toPath);
-            chunkManager.removeAllChunks(sourcefile);
-        }
-
-
+        FileMerger fileMerger = new FileMerger();
+        DownloadController downloadController = new DownloadController(5, peerController, sourcefileController, chunkManager, fileMerger);
+        UI ui = new UI(new Scanner(System.in));
+        int port = 12345;
+        new MainController(port, peerController, sourcefileController, downloadController, ui).startController();
     }
 
 }
